fea6e9f17de9b7709f463b2a04800ca695766c02
dineshDevops45 <dineshkumarmanickam45@gmail.com>
2026-05-21 16:33:43 +0530

    Fixed Razor page and updated homepage UI

--- obj/Debug/net8.0/teamcity-demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/teamcity-demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teamcity-demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d866424601de2a5b5d57346acfa708cea24d3a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("teamcity-demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teamcity-demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
